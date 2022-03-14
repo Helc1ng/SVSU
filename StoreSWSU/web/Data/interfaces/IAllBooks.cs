478cfa5eff33e0b57b0d97cd0a364c409eeca17a
Helc1ng <101250881+Helc1ng@users.noreply.github.com>
2022-03-15 00:29:29 +0300

    Add files via upload

---
+++ StoreSWSU/web/Data/interfaces/IAllBooks.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using web.Data.Models;
+
+namespace web.Data.interfaces
+{
+    public interface IAllBooks
+    {
+        IEnumerable<Book> Book { set; get; }
+        IEnumerable<Book> getFavBook { set; get; }
+        Book getObjectbook(int bookId);
+    }
+}
---
+++ StoreSWSU/web/Data/interfaces/IBookCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using web.Data.Models;
+
+namespace web.Data.interfaces
+{
+    public class IBookCategory
+    {
+        IEnumerable<Category> AllCategories { get; }
+    }
+}
